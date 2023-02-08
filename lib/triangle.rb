6217a29925037572f4f98bec05bcc4242c040b46
Andrew <andrewonulak@gmail.com>
2023-02-08 15:58:29 -0700

    commit tree 26e190f518d50b7ce2cd659590c3af2194958edb

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
-  # write code here
+  
+  def initialize a, b, c
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if (
+      (@a + @b > @c) &&
+      (@a + @c > @b) &&
+      (@b + @c > @a)
+    )
+      if (@a > 0 && @b > 0 && @c > 0)
+        if (@a == @b && @a == @c && @b == @c)
+          :equilateral
+        elsif (
+          (@a == @b && @a != @c) || 
+          (@b == @c && @b != @a) ||
+          (@c == @a && @c != @b)
+        )
+          :isosceles
+        else
+          :scalene
+        end
+      elsif
+        raise TriangleError
+      end
+    elsif
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+    
+  end
 end
